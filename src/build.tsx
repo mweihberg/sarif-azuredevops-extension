b9ac1aceeb34c18fe0739ea6fbce254ddd138b96
nv\weihbergmartinitn <Martin.Weihberg@it.niedersachsen.de>
2024-10-02 07:50:30 +0200

    - Anzeigen vollständiger Dateinamen - Rausschmiss Application Insights

--- src/build.tsx
+++ src/build.tsx
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 import { Viewer } from '@microsoft/sarif-web-component'
-import { ApplicationInsights } from "@microsoft/applicationinsights-web"
 import { CommonServiceIds, getClient, IProjectPageService } from 'azure-devops-extension-api'
 import { BuildRestClient, BuildServiceIds, IBuildPageDataService } from 'azure-devops-extension-api/Build'
 import * as SDK from 'azure-devops-extension-sdk'
@@ -14,16 +13,6 @@ import { Log } from 'sarif'
 import { getArtifactsFileEntries } from './build.getArtifactsFileEntries'
 
 const perfLoadStart = performance.now() // For telemetry.
-
-const appInsights = new ApplicationInsights({
-	config: {
-		connectionString: CONNECTION_STRING,
-	},
-})
-appInsights.loadAppInsights()
-addEventListener('unhandledrejection', e => appInsights.trackException({
-	exception: e.reason
-}))
 
 @observer class Tab extends React.Component {
 	@observable.ref logs = undefined as Log[]
@@ -37,143 +26,96 @@ addEventListener('unhandledrejection', e => appInsights.trackException({
 			applyTheme: true,
 			loaded: true,
 		})
-		;(async () => {
-			await SDK.ready()
+			; (async () => {
+				await SDK.ready()
 
-			const user = SDK.getUser()
-			const organization = SDK.getHost().name
-			const accessToken = await SDK.getAccessToken();
-			const identitiesUri = `https://vssps.dev.azure.com/${organization}/_apis/identities?searchFilter=General&filterValue=${user.name}&queryMembership=None&api-version=7.0`
+				const user = SDK.getUser()
+				const organization = SDK.getHost().name
+				const accessToken = await SDK.getAccessToken();
+				const identitiesUri = `https://vssps.dev.azure.com/${organization}/_apis/identities?searchFilter=General&filterValue=${user.name}&queryMembership=None&api-version=7.0`
 
-			try {
-				const response = await fetch(identitiesUri, {
-					method: "GET",
-					mode: "cors",
-					headers: {
-						"Accept": "application/json",
-						"Authorization": `Bearer ${accessToken}`,
-						"X-VSS-ReauthenticationAction": "Suppress",
+				const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService)
+				const project = await projectService.getProject()
+
+				const buildPageDataService = await SDK.getService<IBuildPageDataService>(BuildServiceIds.BuildPageDataService)
+				const buildPageData = await buildPageDataService.getBuildPageData()
+				if (!buildPageData) {
+					SDK.notifyLoadSucceeded()
+					return
+				}
+				const { build, definition } = buildPageData
+
+				const buildClient = getClient(BuildRestClient)
+
+				const files = await getArtifactsFileEntries(buildClient, project.id, build.id)
+
+				const logTexts = await Promise.all(files.map(async file => {
+					let contents = await file.contentsPromise
+					if (contents.match(/^\uFEFF/)) {
+						contents = contents.replace(/^\uFEFF/, ''); // Trim BOM to avoid 'Unexpected token ﻿ in JSON at position 0'.
 					}
+					return contents
+				}))
+
+				const logs = logTexts.map(log => {
+					if (log === '') {
+						return undefined
+					}
+					try {
+						return JSON.parse(log) as Log
+					} catch (e) {
+						return undefined
+					}
+				}).filter(log => log)
+
+				// Make sure each run has a property bag
+				logs.forEach(log => log.runs.forEach(run => run.properties = run.properties || {}))
+
+				const toolNames = logs.map(log => {
+					return log.runs
+						.filter(run => run.tool.driver) // Guard against old versions.
+						.map(run => run.tool.driver.name)
 				})
+				const toolNamesSet = new Set([].concat(...toolNames))
 
-				if (response.ok) {
-					const json = await response.json()
-					this.tenant = json.value[0].properties["Domain"].$value
-				} else {
-					appInsights.trackTrace({ message: 'Failed to get tenant' }, {
-						status: response.status.toString(),
-						message: await response.text(),
-					})
+				// Show file names when the tool names are homogeneous.
+				if (files.length > 1) {
+					logs.forEach((log, i) =>
+						log.runs.forEach(run => {
+							run.properties = run.properties || {}
+							run.properties['logFileName'] = files[i].name
+						})
+					)
 				}
-			} catch (e) {
-				appInsights.trackException({ exception: e })
-			}
 
-			if (this.tenant === '72f988bf-86f1-41af-91ab-2d7cd011db47') {
-				appInsights.setAuthenticatedUserContext(user.name, organization)
-			}
-
-			const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService)
-			const project = await projectService.getProject()
-
-			const buildPageDataService = await SDK.getService<IBuildPageDataService>(BuildServiceIds.BuildPageDataService)
-			const buildPageData = await buildPageDataService.getBuildPageData()
-			if (!buildPageData) {
-				SDK.notifyLoadSucceeded()
-				appInsights.trackException({ exception: new Error('buildPageData undefined') })
-				return
-			}
-			const { build, definition } = buildPageData
-
-			const buildClient = getClient(BuildRestClient)
-
-			const files = await getArtifactsFileEntries(buildClient, project.id, build.id)
-
-			const logTexts = await Promise.all(files.map(async file => {
-				let contents = await file.contentsPromise
-				if (contents.match(/^\uFEFF/)) {
-					appInsights.trackTrace({ message: 'BOM trimmed' })
-					contents = contents.replace(/^\uFEFF/, ''); // Trim BOM to avoid 'Unexpected token ﻿ in JSON at position 0'.
-				}
-				return contents
-			}))
-
-			const logs = logTexts.map(log => {
-				if (log === '') {
-					appInsights.trackTrace({ message: 'Empty log' })
-					return undefined
-				}
-				try {
-					return JSON.parse(log) as Log
-				} catch(e) {
-					appInsights.trackException({
-						exception: e,
-						properties: {
-							logSnippet: JSON.stringify(log.slice(0, 100))
-						}
-					})
-					return undefined
-				}
-			}).filter(log => log)
-
-			// Make sure each run has a property bag
-			logs.forEach(log => log.runs.forEach(run => run.properties = run.properties || {}))
-
-			const toolNames = logs.map(log => {
-				return log.runs
-					.filter(run => run.tool.driver) // Guard against old versions.
-					.map(run => run.tool.driver.name)
-			})
-			const toolNamesSet = new Set([].concat(...toolNames))
-
-			// Show file names when the tool names are homogeneous.
-			if (files.length > 1 && toolNamesSet.size === 1) {
+				const buildProps = await buildClient.getBuild(project.name, build.id)
 				logs.forEach((log, i) =>
 					log.runs.forEach(run => {
-						run.properties = run.properties || {}
-						run.properties['logFileName'] = files[i].name
+						// Add a versionControlProvenance if one is not already present.
+						if (!run.versionControlProvenance?.[0]) {
+							run.versionControlProvenance = [{
+								repositoryUri: buildProps.repository.url,
+								revisionId: buildProps.sourceVersion,
+							}];
+						}
+
+						// Metadata for use by the web component.
+						run.properties['artifactName'] = files[i].artifactName
+						run.properties['filePath'] = files[i].filePath
+						run.properties['buildId'] = files[i].buildId
 					})
 				)
-			}
 
-			const buildProps = await buildClient.getBuild(project.name, build.id)
-			logs.forEach((log, i) =>
-				log.runs.forEach(run => {
-					// Add a versionControlProvenance if one is not already present.
-					if (!run.versionControlProvenance?.[0]) {
-						run.versionControlProvenance = [{
-							repositoryUri: buildProps.repository.url,
-							revisionId: buildProps.sourceVersion,
-						}];
-					}
-
-					// Metadata for use by the web component.
-					run.properties['artifactName'] = files[i].artifactName
-					run.properties['filePath'] = files[i].filePath
-					run.properties['buildId'] = files[i].buildId
+				runInAction(() => {
+					this.logs = logs
+					this.toolNames = toolNamesSet
+					this.pipelineId = `${organization}.${definition.id}`
+					this.user = user.name
 				})
-			)
 
-			runInAction(() => {
-				this.logs = logs
-				this.toolNames = toolNamesSet
-				this.pipelineId = `${organization}.${definition.id}`
-				this.user = user.name
-			})
+				SDK.notifyLoadSucceeded()
 
-			SDK.notifyLoadSucceeded()
-
-			appInsights.trackPageView({
-				name: 'Build',
-				properties: {
-					duration: performance.now() - perfLoadStart,
-					results: logs.reduce((accum, log) => accum + log.runs.reduce((accum, run) => accum + run.results?.length ?? 0, 0), 0),
-					logs: logs.length,
-					toolNames: toolNamesSet.values(),
-					version: SDK.getExtensionContext().version,
-				},
-			})
-		})()
+			})()
 	}
 
 	render() {
@@ -185,7 +127,7 @@ addEventListener('unhandledrejection', e => appInsights.trackException({
 				filterState={{
 					Baseline: { value: ['new', 'updated', 'absent'] }, // Focusing on incremental changes.
 					Level: { value: ['error', 'warning'] },
-					Suppression: { value: ['unsuppressed']},
+					Suppression: { value: ['unsuppressed'] },
 				}}
 				user={user}
 				showActions={this.tenant === '72f988bf-86f1-41af-91ab-2d7cd011db47'}
@@ -194,7 +136,7 @@ addEventListener('unhandledrejection', e => appInsights.trackException({
 			: <div className="full">
 				No SARIF logs found. Logs must be placed within an Artifact named "CodeAnalysisLogs".
 				<a href="https://learn.microsoft.com/en-us/azure/devops/pipelines/artifacts/pipeline-artifacts?view=azure-devops&tabs=yaml" target="_blank" className='noArtifactLearnMore'>Learn more</a>
-			  </div>
+			</div>
 	}
 }
 
--- src/workItem.tsx
+++ src/workItem.tsx
@@ -1,28 +1,17 @@
-import { ApplicationInsights } from "@microsoft/applicationinsights-web"
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {observable} from 'mobx'
-import {observer} from 'mobx-react'
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 
 import 'script-loader!vss-web-extension-sdk/lib/VSS.SDK.min.js'
 declare var VSS: any
 // import {VSS} from './VSS.SDK.mock'
 
-import { Viewer} from '@microsoft/sarif-web-component'
+import { Viewer } from '@microsoft/sarif-web-component'
 import { calcToolNamesSet } from './calcToolNamesSet'
 import { Log } from 'sarif'
 
 const perfLoadStart = performance.now() // For telemetry.
-
-const appInsights = new ApplicationInsights({
-	config: {
-		connectionString: CONNECTION_STRING,
-	},
-})
-appInsights.loadAppInsights()
-addEventListener('unhandledrejection', e => appInsights.trackException({
-	exception: e.reason
-}))
 
 @observer class Tab extends React.Component {
 	static decoder = new TextDecoder() // @sinonjs/text-encoding polyfills IE.
@@ -34,7 +23,7 @@ addEventListener('unhandledrejection', e => appInsights.trackException({
 			explicitNotifyLoaded: true,
 		})
 		VSS.require(['TFS/WorkItemTracking/RestClient'], witModule => { // Tfs/WebPlatform/Client/TFS/WorkItemTracking/RestClient.ts
-			const onLoaded = async ({id}) => {
+			const onLoaded = async ({ id }) => {
 				const witClient = witModule.getClient()
 				const workItem = await witClient.getWorkItem(id, null, null, 1)
 				const files = (workItem.relations || [])
@@ -50,30 +39,19 @@ addEventListener('unhandledrejection', e => appInsights.trackException({
 				const logTexts = await Promise.all(files.map(async file => await file.sarif())) as string[]
 				const logs = logTexts.map(log => JSON.parse(log) as Log)
 				this.logs = logs
-
-				appInsights.trackPageView({
-					name: 'WorkItem',
-					properties: {
-						duration: performance.now() - perfLoadStart,
-						results: logs.reduce((accum, log) => accum + log.runs.reduce((accum, run) => accum + run.results?.length ?? 0, 0), 0),
-						logs: logs.length,
-						toolNames: calcToolNamesSet(logs).values(),
-						version: VSS.getExtensionContext().version,
-					},
-				})
 			}
 			VSS.register(VSS.getContribution().id, { onLoaded }) // ;onLoaded({ id: 1 })
 			VSS.notifyLoadSucceeded() // Not working within onLoaded()
 		})
 	}
 	render() {
-		const {logs} = this
+		const { logs } = this
 		return !logs || logs.length
 			? <Viewer logs={logs} showSuppression
 				filterState={{
 					Baseline: { value: ['new', 'updated', 'unchanged'] }, // Not focusing on incremental changes, focusing on current state.
 					Level: { value: ['error', 'warning'] },
-					Suppression: { value: ['unsuppressed']},
+					Suppression: { value: ['unsuppressed'] },
 				}} showActions={false} />
 			: <div className="full">No SARIF attachments found.</div>
 	}
